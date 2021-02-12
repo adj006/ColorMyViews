5dab2db5bec855481e1cb5485d1f6378dc309d3b
Adrian <ajim1989@gmail.com>
2021-02-11 20:51:10 -0800

    Added 3 buttons to change boxes colors

--- app/src/main/java/com/example/android/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/android/colormyviews/MainActivity.kt
@@ -4,25 +4,25 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.example.android.colormyviews.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setListeners()
     }
 
     private fun setListeners() {
-        val box_one_text = findViewById<View>(R.id.box_one_text)
-        val box_two_text = findViewById<View>(R.id.box_two_text)
-        val box_three_text = findViewById<View>(R.id.box_three_text)
-        val box_four_text = findViewById<View>(R.id.box_four_text)
-        val box_five_text = findViewById<View>(R.id.box_five_text)
-        val constraint_layout = findViewById<View>(R.id.constraint_layout)
-
         val clickableViews: List<View> =
-                listOf(box_one_text, box_two_text, box_three_text,
-                        box_four_text, box_five_text, constraint_layout)
+                listOf(binding.boxOneText, binding.boxTwoText, binding.boxThreeText,
+                        binding.boxFourText, binding.boxFiveText, binding.constraintLayout,
+                        binding.redButton, binding.yellowButton, binding.greenButton)
 
         for (item in clickableViews) {
              item.setOnClickListener { makeColored(it) }
@@ -30,14 +30,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun makeColored(view: View) {
-        when (view.id) {
+        when (view) {
             // Boxes using Color class colors for background
-            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
-            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+            binding.boxOneText -> view.setBackgroundColor(Color.DKGRAY)
+            binding.boxTwoText -> view.setBackgroundColor(Color.GRAY)
             // Boxes using Android color resources for background
-            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
-            R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
-            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+            binding.boxThreeText -> view.setBackgroundResource(android.R.color.holo_green_light)
+            binding.boxFourText -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            binding.boxFiveText -> view.setBackgroundResource(android.R.color.holo_green_light)
+            // Boxes using custom colors for background
+            binding.redButton -> binding.boxThreeText.setBackgroundResource(R.color.my_red)
+            binding.yellowButton -> binding.boxFourText.setBackgroundResource(R.color.my_yellow)
+            binding.greenButton -> binding.boxFiveText.setBackgroundResource(R.color.my_green)
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
